26d4c787519c0e412323fb568cdeadf51920e09e
unknown <iandunca99@gmail.com>
2026-02-28 14:01:39 +0300

    changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,29 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'http', hostname: 'localhost', port: '9000', pathname: '/**' },
-      { protocol: 'https', hostname: '**' }
+      { 
+        protocol: 'http', 
+        hostname: 'localhost', 
+        port: '9000', 
+        pathname: '/**' 
+      },
+      { 
+        protocol: 'https', 
+        hostname: '**' // This allows ALL https images (be careful in production!)
+      }
     ]
-  }
+  },
+  // This must be INSIDE the nextConfig object
+  experimental: {
+    allowedDevOrigins: [
+      "http://165.232.102.141",
+      "https://165.232.102.141",
+      "http://mombasaunited.com",
+      "https://mombasaunited.com",
+      "http://www.mombasaunited.com",
+      "https://www.mombasaunited.com",
+    ],
+  },
 };
+
 module.exports = nextConfig;
