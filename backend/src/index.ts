86cafcd5b9e5e9ec3e1f5296373bf57ba0e3f2b4
unknown <iandunca99@gmail.com>
2026-03-10 14:05:50 +0300

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -50,7 +50,8 @@ app.use(
   })
 );
 app.options("*", cors());
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(morgan("dev"));
 
 app.get("/health", async (_req, res) => {
